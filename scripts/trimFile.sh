4fb43c1c989dd934c97468ab4b964c29ab8108cf
sp1tz <42953063+sp1tz@users.noreply.github.com>
2020-02-12 18:39:12 -0500

    Sort each file.

--- scripts/trimFile.sh
+++ scripts/trimFile.sh
@@ -2,6 +2,7 @@
 # This script is for initial cleaning of the data.
 #  For the LUAD set it removes the one header line and left most column.
 #  For all others it removes the two header lines.
+#  Every file is sorted according to the same ordering.
 #
 # To run from the repository root:
 #  W/ GNU PARALLEL (https://www.gnu.org/software/parallel/):
@@ -10,10 +11,12 @@
 #    time -p find ./data -name "*.txt" -exec ./scripts/trimFile.sh {} \;
 if [[ -n $1 ]]; then
   if [[ $1 == *"un"* ]]; then # LUAD has different format.
-    tail -n +2 "$1" | cut -f2- > "$1"_tmp.txt && mv "$1"_tmp.txt "$1"
+    tail -n +2 "$1" | cut -f2- | sort > "$1"_tmp && mv "$1"_tmp "$1"
   else
     if [[ $1 != *"2991"* ]]; then # Strangely there's one file already trimmed.
-      tail -n +3 "$1" > "$1"_tmp && mv "$1"_tmp "$1"
+      tail -n +3 "$1" | sort > "$1"_tmp && mv "$1"_tmp "$1"
+    else
+      sort -o "$1" "$1"
     fi
   fi
 fi
